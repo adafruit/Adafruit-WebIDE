b04724693aa29b9f7c2ed071df1e824b89b9e760
Justin Cooper <jwcooper@gmail.com>
2012-08-29 16:47:41 -0500

    bunch of new editor features, create files and folders

--- public/javascripts/occ_editor.js
+++ public/javascripts/occ_editor.js
@@ -22,6 +22,18 @@
                                     '<form id="clone-repository-form" method="post" action="/create/repository">' +
                                       '<label for="repository_url">Remote Repository URL:</label>' +
                                       '<input name="repository_url" type="text">' +
+                                    '</form>',
+    "create_project_folder":        '<form id="create-project-form">' +
+                                      '<label for="folder_name">Project Name:</label>' +
+                                      '<input name="folder_name" type="text">' +
+                                      '<button class="btn btn-mini create-cancel" type="button">Cancel</button>' +
+                                      '<input class="btn btn-mini create-save" type="submit" text="Save" />' +
+                                    '</form>',
+    "create_file_folder":        '<form id="create-file-form">' +
+                                      '<label for="file_name">File Name:</label>' +
+                                      '<input name="file_name" type="text">' +
+                                      '<button class="btn btn-mini create-cancel" type="button">Cancel</button>' +
+                                      '<input class="btn btn-mini create-save" type="submit" text="Save" />' +
                                     '</form>'
   };
 
@@ -76,6 +88,7 @@
       build_navigator_top(list[0]);
       build_navigator_list(list);
       build_navigator_bottom(list[0]);
+      handle_navigator_scroll();
     }
 
     davFS.listDir(path, populateFileSystem);
@@ -234,6 +247,24 @@
     $(document).mouseup(handle_dragbar_mouseup);
   }
 
+  function handle_navigator_scroll() {
+    //pretty ugly, but seems to work in firefox and chrome so far
+    var nav_height = $('#navigator').outerHeight();
+    var nav_footer_height = $('#navigator-bottom').outerHeight();
+    var nav_top_height = $('#navigator-top').outerHeight();
+    var nav_folder_height = $('#navigator-folder').outerHeight();
+    //minor hack to force an accurate scrollheight;
+    $('#navigator ul').height(0);
+    var nav_list_height = $('#navigator ul').prop('scrollHeight');
+    var possible_height = nav_height - (nav_footer_height + nav_top_height + nav_folder_height);
+
+    if (nav_list_height < possible_height) {
+      $('#navigator ul').height(nav_list_height);
+    } else {
+      $('#navigator ul').height(possible_height - 4);
+    }
+  }
+
   function handle_navigator_actions() {
     function navigator_item_selected(event) {
       event.preventDefault();
@@ -257,7 +288,18 @@
 
     function navigator_create_selected(event) {
       event.preventDefault();
-      $('#create-modal').modal('show');
+      var link_text = $('a', this).text();
+
+      if (/repository/i.test(link_text)) {
+        $('#create-modal').modal('show');
+      } else if (/project/i.test(link_text)) {
+        $(this).data('link', $(this).html()).html(templates.create_project_folder);
+        $('input[name="folder_name"]').focus();
+      } else if (/file/i.test(link_text)) {
+        $(this).data('link', $(this).html()).html(templates.create_file_folder);
+        $('input[name="file_name"]').focus();
+      }
+      handle_navigator_scroll();
     }
 
     function create_modal_submit(event) {
@@ -267,29 +309,76 @@
       if ($form.attr('id') === "clone-repository-form") {
         clone_repository($form);
       }
+    }
 
+    function create_cancel(event) {
+      var $parent = $(this).closest('p');
+      create_replace($parent);
+    }
+
+    function create_replace($element) {
+      var link = $element.data('link');
+      $element.replaceWith('<p class="navigator-item-create">' + link + '</p>');
+      handle_navigator_scroll();
+    }
+
+    function create_fs_response(err, status) {
+      var $create_wrapper = $('.navigator-item-create');
+      var folder_name = $('input[name="folder_name"]').val();
+      var parent_folder = $('.navigator-item-back').data("file");
+      if (err) {
+        if (!$create_wrapper.find('.error').length) {
+          $create_wrapper.prepend($('<span class="error">' + err + '</span>'));
+        } else {
+          $('.error', $create_wrapper).replaceWith($('<span class="error">' + err + '</span>'));
+        }
+        handle_navigator_scroll();
+      } else {
+        create_replace($create_wrapper);
+        occEditor.populate_navigator(parent_folder.path);
+        occEditor.populate_editor_bar();
+      }
+
+    }
+
+    function create_folder(event) {
+      event.preventDefault();
+      var $create_wrapper = $('.navigator-item-create');
+      var folder_name = $('input[name="folder_name"]').val();
+      var parent_folder = $('.navigator-item-back').data("file");
+
+      davFS.mkDir(parent_folder.path + folder_name, create_fs_response);
+    }
+
+    function create_file() {
+      event.preventDefault();
+      var $create_wrapper = $('.navigator-item-create');
+      var file_name = $('input[name="file_name"]').val();
+      var parent_folder = $('.navigator-item-back').data("file");
+
+      davFS.write(parent_folder.path + file_name, '', create_fs_response);
     }
     //clicking a file or folder in the list.
     $(document).on('click touchstart', '.navigator-item', navigator_item_selected);
     $(document).on('click touchstart', '.navigator-item-back', navigator_back_selected);
     $(document).on('click touchstart', '.navigator-item-create', navigator_create_selected);
     $(document).on('click touchstart', '#create-modal .modal-submit', create_modal_submit);
+    $(document).on('click touchstart', '#create-project-form .create-save', create_folder);
+    $(document).on('click touchstart', '#create-project-form .create-cancel', create_cancel);
+    $(document).on('click touchstart', '#create-file-form .create-save', create_file);
+    $(document).on('click touchstart', '#create-file-form .create-cancel', create_cancel);
   }
 
   function clone_repository($form) {
     function handler(err, data, jqXHR) {
       $('.modal-submit').removeClass('disabled');
       if (jqXHR.status === 200) {
-        console.log('here');
         $('#create-modal').modal('hide');
         occEditor.populate_navigator();
         occEditor.populate_editor_bar();
       } else {
         $('#clone-repository-form').prepend('<span class="error">' + jqXHR.responseText + '</span>');
       }
-      console.log(err);
-      console.log(data);
-      console.log(jqXHR);
     }
 
     var request = $.ajax({
