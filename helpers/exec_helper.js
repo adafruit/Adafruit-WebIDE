4ad6adef014412af9e45e30e0279ab0797fe5f8f
Justin Cooper <jwcooper@gmail.com>
2012-09-04 15:35:34 -0500

    add javascript/node support

--- helpers/exec_helper.js
+++ helpers/exec_helper.js
@@ -7,41 +7,36 @@ exports.execute_program = function(file, socket) {
     execute_python(file_path, socket);
   } else if (file.extension === 'rb') {
     execute_ruby(file_path, socket);
+  } else if (file.extension === 'js') {
+    execute_javascript(file_path, socket);
   }
-}
+};
 
 function execute_python(file, socket) {
   console.log('execute_python');
-  var prog = spawn('python', [file]);
-
-  prog.stdout.on('data', function(data) {
-    socket.emit('program-stdout', {output: data.toString()});
-  });
-
-  prog.stderr.on('data', function(data) {
-    socket.emit('program-stderr', {output: data.toString()});
-  });
-
-  prog.on('exit', function(code) {
-    socket.emit('program-exit', {code: code});
-  });
+  execute_program(file, socket, "python");
 }
 
 function execute_ruby(file, socket) {
-  var prog = spawn('ruby', [file]);
+  execute_program(file, socket, "ruby");
+}
+
+function execute_javascript(file, socket) {
+  execute_program(file, socket, "node");
+}
+
+function execute_program(file, socket, type) {
+  var prog = spawn(type, [file]);
 
   prog.stdout.on('data', function(data) {
-    console.log(data.toString());
     socket.emit('program-stdout', {output: data.toString()});
   });
 
   prog.stderr.on('data', function(data) {
     socket.emit('program-stderr', {output: data.toString()});
-  });  
+  });
 
   prog.on('exit', function(code) {
-    console.log(code);
     socket.emit('program-exit', {code: code});
   });
 }
-
