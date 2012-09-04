8cb483212b3a6885d0cc3bd98cfb8e1ed403106b
Justin Cooper <jwcooper@gmail.com>
2012-09-04 15:35:16 -0500

    setup and submit for setup page

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,6 @@
+var redis = require("redis"),
+    client = redis.createClient();
+
 exports.login = function(req, res){
   res.render('users/login', { title: 'test', user: req.user });
 };
@@ -5,4 +8,19 @@ exports.login = function(req, res){
 exports.logout = function(req, res){
   req.logout();
   res.redirect('/');
+};
+
+exports.setup = function(req, res) {
+  res.render('users/setup');
+};
+
+exports.submit_setup = function(req, res) {
+  console.log(req.body.key);
+  console.log(req.body.secret);
+
+  if (req.body.key && req.body.secret) {
+    client.hmset("bitbucket_oauth", "consumer_key", req.body.key, "consumer_secret", req.body.secret, function() {
+      res.redirect('/login');
+    });
+  }
 };
