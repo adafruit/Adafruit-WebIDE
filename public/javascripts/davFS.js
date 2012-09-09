44d78fff135b9ce60237c59b85f87c2dae0cd0c3
Justin Cooper <jwcooper@gmail.com>
2012-09-09 11:28:46 -0500

    remove console.log

--- public/javascripts/davFS.js
+++ public/javascripts/davFS.js
@@ -9,7 +9,6 @@ $.fn.filterNode = function(name) {
   //Public Methods
   davFS.listDir = function(path, cb) {
     function filter(item) {
-      console.log(item);
       var filter_list = ['.git', 'DS_Store', 'pyc'];
       if ($.inArray(item.name, filter_list) !== -1) return true;
       if ($.inArray(item.extension, filter_list) !== -1) return true;
