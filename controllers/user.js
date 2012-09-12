ffdf667fe2c4b67679ea5fa87cb181ba9cba9f80
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-09-11 22:19:45 -0500

    add git config variables to user

--- controllers/user.js
+++ controllers/user.js
@@ -11,16 +11,41 @@ exports.logout = function(req, res){
 };
 
 exports.setup = function(req, res) {
-  res.render('users/setup');
+  client.hgetall('bitbucket_oauth', function (err, bitbucket) {
+    client.hgetall('user', function (err, user) {
+      var locals = {
+        consumer_key: "",
+        consumer_secret: "",
+        full_name: "",
+        email: ""
+      };
+      if (bitbucket) {
+        locals.consumer_key = bitbucket.consumer_key;
+        locals.consumer_secret = bitbucket.consumer_secret;
+      }
+
+      if (user) {
+        locals.name = user.name;
+        locals.email = user.email;
+      }
+
+      console.log(locals);
+      res.render('users/setup', locals);
+    });
+  });
 };
 
 exports.submit_setup = function(req, res) {
   console.log(req.body.key);
   console.log(req.body.secret);
+  console.log(req.body.full_name);
+  console.log(req.body.email);
 
   if (req.body.key && req.body.secret) {
     client.hmset("bitbucket_oauth", "consumer_key", req.body.key, "consumer_secret", req.body.secret, function() {
-      res.redirect('/login');
+      client.hmset("user", "name", req.body.name, "email", req.body.email, function() {
+        res.redirect('/login');
+      });
     });
   }
 };
