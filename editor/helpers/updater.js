89f8fd331e5c62289adb562ebbb4868487152362
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-09-13 23:30:06 -0500

    basic updating support

--- editor/helpers/updater.js
+++ editor/helpers/updater.js
@@ -1,13 +1,28 @@
-var exec = require('child_process').exec;
+var exec = require('child_process').exec,
+  fs = require ('fs'),
+  request = require('request');
+
+function download_archive(cb) {
+  var download = request('https://dl.dropbox.com/s/wfg79l5f4old9pc/editor.tar.gz?dl=1');
+  download.pipe(fs.createWriteStream(__dirname + '/../../editor.tar.gz'));
+  download.on('error', function (e) {
+    cb(e);
+  });
+  download.on('end', function () {
+      return cb();
+  });
+}
 
 exports.execute_program = function execute_program() {
-  var editor_zip = __dirname + "/../../editor_v1.tar.bz2";
-  console.log(editor_zip);
-  execute(editor_zip);
+  download_archive(function() {
+    var editor_zip = __dirname + "/../../editor.tar.gz";
+    console.log(editor_zip);
+    execute(editor_zip);
+  });
 };
 
 function execute(file) {
-  var child = exec('tar -jxvf ' + file + ' -C ' + __dirname + '/../../editor',
+  var child = exec('tar -zxvf ' + file + ' -C ' + __dirname + '/../../',
     function (error, stdout, stderr) {
       console.log('stdout: ' + stdout);
       console.log('stderr: ' + stderr);
@@ -17,4 +32,4 @@ function execute(file) {
   });
 }
 
-this.execute_program();
+//this.execute_program();
