0ba87ce57d3c361333c345baf312f055085b6fea
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-09-25 10:07:54 -0500

    remove unused code, and sanitize repository creation

--- editor/controllers/editor.js
+++ editor/controllers/editor.js
@@ -1,6 +1,8 @@
 var fs_helper = require('../helpers/fs_helper'),
     git_helper = require('../helpers/git_helper'),
-    config = require('../config/config');
+    config = require('../config/config'),
+    check = require('validator').check,
+    sanitize = require('validator').sanitize;
 
 //Loads the editor
 exports.index = function(req, res) {
@@ -8,29 +10,10 @@ exports.index = function(req, res) {
 };
 
 exports.create_repository = function(req, res) {
-  //TODO this is clearly, very, very insecure :)
-  var repository_url = req.body.repository_url;
+  var repository_url = sanitize(req.body.repository_url).xss().trim();
 
   git_helper.clone_update_remote_push(req.user, repository_url, function(err, status) {
     if (err) res.send(err, 404);
     else res.send(status, 200);
-  });
-};
-
-exports.filesystem = function(req, res){
-  var repository = req.query.repository;
-  console.log(repository);
-  //repository = "Adafruit-Raspberry-Pi-Python-Code";
-  fs_helper.read_repository(repository, function(results) {
-    res.send(results);
-  });
-};
-
-exports.file = function(req, res) {
-  //TODO this is clearly, very, very insecure :)
-  var path = req.query.path;
-  //console.log(path);
-  fs_helper.open_file(path, function(results) {
-    res.send(results);
   });
 };
