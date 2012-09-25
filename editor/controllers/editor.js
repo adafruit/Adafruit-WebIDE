b97a9d7fdb09a35af5050de43eaa41ee405fa189
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-09-25 15:17:25 -0500

    add new function for upload route

--- editor/controllers/editor.js
+++ editor/controllers/editor.js
@@ -1,4 +1,5 @@
 var fs_helper = require('../helpers/fs_helper'),
+    path = require('path'),
     git_helper = require('../helpers/git_helper'),
     config = require('../config/config'),
     check = require('validator').check,
@@ -15,5 +16,20 @@ exports.create_repository = function(req, res) {
   git_helper.clone_update_remote_push(req.user, repository_url, function(err, status) {
     if (err) res.send(err, 404);
     else res.send(status, 200);
+  });
+};
+
+exports.upload_file = function(req, res) {
+  console.log(req.files.files[0]);
+
+  var temp_path = sanitize(req.files.files[0].path).xss().trim();
+
+  var folder_path = sanitize(req.body.path).xss().trim().replace('filesystem', 'repositories');
+  var new_path = __dirname + '/../..' + folder_path + sanitize(req.files.files[0].name).xss().trim();
+  new_path = path.resolve(new_path);
+
+  console.log(temp_path, new_path);
+  fs_helper.move_uploaded_file(temp_path, new_path, function(err, status) {
+    res.send(true, 200);
   });
 };
