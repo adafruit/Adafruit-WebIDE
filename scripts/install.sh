2b1dbb0848fee678a4686fdc8b15a600cff7e4df
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-10-04 12:07:34 -0500

    add latest version to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,7 +17,7 @@ mkdir -p "$WEBIDE_ROOT"
 cd "$WEBIDE_ROOT"
 
 echo "**** Downloading the latest version of the WebIDE ****"
-curl -s https://dl.dropbox.com/s/7xhlca346otrw3v/editor-0.1.0.tar.gz | tar xzf -
+curl -s https://raw.github.com/adafruit/Adafruit-WebIDE/release/release/editor.tar.gz | tar xzf -
 
 echo "**** Installing required libraries (node, npm, redis-server) ****"
 sudo apt-get install nodejs npm redis-server -y
