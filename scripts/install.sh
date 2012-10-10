4a44cfcdc02781a22f7aceac189d9a68d82fe3ea
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-10-09 23:48:09 -0500

    update install scripts to handle webide user, and add default .bashrc file

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,15 @@
 
 # curl https://raw.github.com/adafruit/Adafruit-WebIDE/release/scripts/install.sh | sudo sh
 
+#tar -zcvf editor-test.tar.gz * --exclude .git --exclude .gitignore
+#scp pi@raspberrypi.local:/home/pi/Adafruit-WebIDE/editor-test.tar.gz editor-test.tar.gz
+#sudo -u webide -g webide node server
+
 set -e
 WEBIDE_ROOT="/usr/share/adafruit/webide"
+
+#needed for SSH key and config access at this point.
+WEBIDE_HOME="/home/webide"
 
 #NODE_PATH="/usr/local/lib/node"
 
@@ -13,21 +20,21 @@ WEBIDE_ROOT="/usr/share/adafruit/webide"
 #fi
 
 mkdir -p "$WEBIDE_ROOT"
+mkdir -p "$WEBIDE_HOME"
 cd "$WEBIDE_ROOT"
 
 echo "**** Downloading the latest version of the WebIDE ****"
-curl -sL https://github.com/downloads/adafruit/Adafruit-WebIDE/editor-0.1.9.tar.gz | tar xzf -
+curl -L https://github.com/downloads/adafruit/Adafruit-WebIDE/editor-0.1.9.tar.gz | tar xzf -
 
 echo "**** Installing required libraries (node, npm, redis-server) ****"
 apt-get install nodejs npm redis-server git -y
 
-rm -rf "$WEBIDE_ROOT/tmp"
-mkdir "$WEBIDE_ROOT/tmp"
-npm config set tmp "$WEBIDE_ROOT/tmp"
-npm install
-cd "$WEBIDE_ROOT/editor"
-npm install
-npm config set tmp /tmp
+echo "**** Create webide user and group ****"
+groupadd webide || true
+useradd -g webide webide || true
+adduser webide i2c || true
+
+chown -R webide:webide "$WEBIDE_HOME"
 chown -R webide:webide "$WEBIDE_ROOT"
 chmod 775 "$WEBIDE_ROOT"
 
@@ -35,9 +42,6 @@ echo "**** Installing the WebIDE as a service ****"
 echo "**** (to uninstall service, execute: 'sudo update-rc.d -f adafruit-webide.sh remove') ****"
 cp "$WEBIDE_ROOT/scripts/adafruit-webide.sh" "/etc/init.d"
 cd /etc/init.d
-groupadd webide
-useradd -g webide webide
-adduser webide i2c
 chmod 755 adafruit-webide.sh
 update-rc.d adafruit-webide.sh defaults
 service adafruit-webide.sh start
