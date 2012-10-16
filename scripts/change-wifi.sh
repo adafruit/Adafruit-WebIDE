19dda0a68f50197326c43f7e8cd94adefcec9e1f
Justin Cooper <jwcooper@gmail.com>
2012-10-16 21:10:00 +0000

    change sed to backup file and make change to wifi

--- scripts/change-wifi.sh
+++ scripts/change-wifi.sh
@@ -3,7 +3,7 @@
 if grep -q wpa-ssid /etc/network/interfaces
 then
   echo "wpa-ssid exists, modifying..."
-  sed "/wpa-ssid/s/\"\([^\"]*\)\"/\"$1\"/" /etc/network/interfaces
+  sed -i.bak "/wpa-ssid/s/\"\([^\"]*\)\"/\"$1\"/" /etc/network/interfaces
 else
   exit 0
 fi
@@ -11,7 +11,7 @@ fi
 if grep -q wpa-psk /etc/network/interfaces
 then
   echo "wpa-psk exists, modifying..."
-  sed "/wpa-psk/s/\"\([^\"]*\)\"/\"$2\"/" /etc/network/interfaces
+  sed -i "/wpa-psk/s/\"\([^\"]*\)\"/\"$2\"/" /etc/network/interfaces
 else
   exit 0
 fi
