4de9dc0a14173b02e9f391c8d63a36051f045d49
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-10-16 23:47:58 -0500

    tweak uninstall

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -7,8 +7,11 @@ WEBIDE_ROOT="/usr/share/adafruit"
 WEBIDE_HOME="/home/webide"
 NODE=$(which node)
 
-read -p "Thank you for using the WebIDE. Uninstall the WebIDE [y/n]? " -n 1 -r
-[[ $REPLY =~ ^[Yy]$ ]] || exit 1
+read -p "Thank you for using the WebIDE. Uninstall the WebIDE [y/n]? " REPLY
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
 
 echo "**** Removing restartd WebIDE configuration ****"
 sed -i '/adafruit-webide.sh/ d' /etc/restartd.conf
