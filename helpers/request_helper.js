e604a1c9b561bfe373f2b514821551af43dadc26
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-10-25 21:14:43 -0500

    set my-pi-projects as default, for already created repos

--- helpers/request_helper.js
+++ helpers/request_helper.js
@@ -30,9 +30,40 @@ exports.post_ssh_key = function(profile, cb) {
 };
 
 /*
+ * Call the Bitbucket OAuth API to createa a repository on the users profile.
+ */
+exports.set_repository_private = function set_repository_private(profile, repository_name) {
+  var username = profile.username;
+  //console.log('set_repository_private');
+  //console.log(profile);
+  var url = "https://api.bitbucket.org/1.0/repositories/" + username + "/" + repository_name;
+  var oauth = { consumer_key: profile.consumer_key,
+                consumer_secret: profile.consumer_secret,
+                token: profile.token,
+                token_secret: profile.token_secret };
+
+  
+  var params = {is_private: true};
+
+  request.put({
+    url:url,
+    body: qs.stringify(params),
+    oauth:oauth
+  }, function (e, r, body) {
+    //console.log(e);
+    console.log(r.statusCode);
+    if (r.statusCode === 200) {
+      console.log("successfully set repository private");
+    }
+    //console.log(body);
+  });
+};
+
+/*
  * Call the Bitbucket OAuth API to get a list of the repositories on the users profile.
  */
 exports.list_repositories = function(profile, cb) {
+  var self = this;
   var url = "https://api.bitbucket.org/1.0/user/repositories/";
   var oauth = { consumer_key: profile.consumer_key,
                 consumer_secret: profile.consumer_secret,
@@ -46,10 +77,19 @@ exports.list_repositories = function(profile, cb) {
   }, function (e, r, body) {
     console.log(e);
     //console.log(r.statusCode);
+    if (body && Array.isArray(body)) {
+      body.forEach(function(repo) {
+        if (repo.slug === "my-pi-projects") {
+          self.set_repository_private(profile, repo.slug);
+        }
+      });
+    }
     //console.log(body);
     cb (e, body);
   });
 };
+
+
 
 /*
  * Call the Bitbucket OAuth API to createa a repository on the users profile.
