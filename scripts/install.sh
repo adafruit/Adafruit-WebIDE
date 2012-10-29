f5ca6b3db405c69917ba0585aa2e8f5440225844
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-10-29 17:24:02 -0500

    install python-smbus as part of installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,9 +30,9 @@ echo "**** Downloading the latest version of the WebIDE ****"
 curl -L https://github.com/downloads/adafruit/Adafruit-WebIDE/editor-0.2.1.tar.gz | tar xzf -
 
 echo "**** Installing required libraries ****"
-echo "**** (nodejs npm redis-server git restartd libcap2-bin avahi-daemon i2c-tools) ****"
+echo "**** (nodejs npm redis-server git restartd libcap2-bin avahi-daemon i2c-tools python-smbus) ****"
 apt-get update
-apt-get install nodejs npm redis-server git restartd libcap2-bin avahi-daemon i2c-tools -y
+apt-get install nodejs npm redis-server git restartd libcap2-bin avahi-daemon i2c-tools python-smbus -y
 
 echo "**** Create webide user and group ****"
 groupadd webide || true
