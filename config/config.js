5edaa56c5d10585d09e82e152ab108ee165bed0b
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2012-11-26 12:03:10 -0600

    bump version to 0.2.4, update changelog

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 //Change port number in /config.
 exports.editor = {
   "port": 80,
-  "version": "0.2.3",
+  "version": "0.2.4",
   "version_url": "https://raw.github.com/adafruit/Adafruit-WebIDE/alpha/release/version.txt",
   "offline": false
 };
