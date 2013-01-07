ce7d0efa25268ae6866333c300c3187da76d1055
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2013-01-07 16:59:56 -0600

    update version information

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 //Change port number in /config.
 exports.editor = {
   "port": 80,
-  "version": "0.2.5",
+  "version": "0.3.0",
   "version_url": "https://raw.github.com/adafruit/Adafruit-WebIDE/alpha/release/version.txt",
   "offline": false
 };
