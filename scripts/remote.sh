ca1df950445aaaa69e1b8e02b91d1539fd4aad27
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2013-02-22 14:55:33 -0600

    remove remote webide tars

--- scripts/remote.sh
+++ scripts/remote.sh
@@ -1,9 +1,11 @@
 cd /home/pi/Adafruit-WebIDE
 git checkout alpha
 git pull origin alpha
+rm webide.tar.gz
 tar -zcvf webide.tar.gz * --exclude .git --exclude .gitignore
 
 cd /home/pi/Adafruit-WebIDE-Update
 git checkout alpha
 git pull origin alpha
+rm webide-update.tar.gz
 tar -zcvf webide-update.tar.gz * --exclude .git --exclude .gitignore
