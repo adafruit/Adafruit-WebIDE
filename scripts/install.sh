bbf124ca3bbd6c55f24c6429e77dc0fb0701991b
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2013-03-07 16:45:01 -0600

    bump version to 0.3.6

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,7 +27,7 @@ mkdir -p "$WEBIDE_HOME"
 cd "$WEBIDE_ROOT"
 
 echo "**** Downloading the latest version of the WebIDE ****"
-curl -L http://adafruit-download.s3.amazonaws.com/webide-0.3.5.tar.gz | tar xzf -
+curl -L http://adafruit-download.s3.amazonaws.com/webide-0.3.6.tar.gz | tar xzf -
 
 echo "**** Installing required libraries ****"
 echo "**** (redis-server git restartd libcap2-bin avahi-daemon i2c-tools python-smbus) ****"
