0398d6c9b0d58f93dc3c71c935472888c7c15bdb
Justin Cooper <jwcooper@Justins-MacBook-Pro.local>
2013-04-19 13:00:39 -0500

    add build_deb to build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ git push origin alpha
 git checkout master
 
 ssh pi@raspberrypi.local 'bash -s' < scripts/remote.sh
+ssh pi@raspberrypi.local 'bash -s' < scripts/build_deb.sh
 
 scp pi@raspberrypi.local:/home/pi/Adafruit-WebIDE/webide.tar.gz webide-$1.tar.gz
 scp pi@raspberrypi.local:/home/pi/Adafruit-WebIDE-Update/webide-update.tar.gz webide-$1-update.tar.gz
